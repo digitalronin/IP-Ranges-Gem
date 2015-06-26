3ffb55e3b683a1cf27e3d6f7417ee3d7ad7a9dc3
David Salgado <david@digitalronin.com>
2015-06-26 11:59:48 +0100

    modernise the ip specs

--- spec/ip-ranges/ip_spec.rb
+++ spec/ip-ranges/ip_spec.rb
@@ -1,94 +1,98 @@
 require 'spec/spec_helper'
 
 describe IpRanges::Ip do
-  before do
-    @ip = IpRanges::Ip.new
-  end
+  let(:number) { "1.200.3.4" }
+  subject(:ip) { described_class.new(:number => number) }
 
-  it "duplicates with a different object" do
-    @ip.number = "1.200.3.4"
-    ip2 = @ip.dup
-    expect(ip2.object_id).to_not eq(@ip.object_id)
-  end
+  context "duplicating" do
+    let(:dup) { ip.dup }
 
-  it "duplicates" do
-    @ip.number = "1.200.3.4"
-    ip2 = @ip.dup
-    expect(ip2).to eq(@ip)
-  end
+    it "duplicates as a different object" do
+      expect(dup.object_id).to_not eq(ip.object_id)
+    end
 
-  it "is greater than or equal to" do
-    @ip.number = "1.200.3.4"
-    eq = IpRanges::Ip.new :number => "1.200.3.4"
-    lt = IpRanges::Ip.new :number => "1.200.3.3"
-    [eq, lt].each {|test| expect(@ip >= test).to be_truthy}
-  end
-
-  it "is equivalent" do
-    @ip.number = "1.200.3.4"
-    ip2 = IpRanges::Ip.new :number => "1.200.3.4"
-    expect(@ip).to eq(ip2)
+    it "considers duplicate to be equivalent" do
+      expect(dup).to eq(ip)
+    end
   end
 
   it "renders to string" do
-    @ip.number = "1.200.3.4"
-    expect(@ip.to_s).to eq("1.200.3.4")
+    expect(ip.to_s).to eq(number)
   end
 
-  it "knows 1.2.3.255 < 1.2.4.0" do
-    @ip.number = '1.2.3.255'
-    ip2 = IpRanges::Ip.new :number => "1.2.4.0"
-    expect((@ip > ip2)).to be_falsey
+  context "comparing" do
+    let(:eql) { IpRanges::Ip.new :number => number      }
+    let(:lt)  { IpRanges::Ip.new :number => "1.200.3.3" }
+
+    it "is greater than or equal to" do
+      [eql, lt].each {|test| expect(ip >= test).to be_truthy}
+    end
+
+    it "is equivalent" do
+      expect(ip).to eq(eql)
+    end
+
+    context "when last tuple rolls over" do
+      let(:number) { "1.2.3.255" }
+      let(:bigger) { described_class.new(:number => '1.2.4.0') }
+
+      specify { expect((ip > bigger)).to be_falsey }
+      specify { expect((bigger >= ip)).to be_truthy }
+    end
+
+    context "when 2nd tuple is bigger" do
+      let(:number) { '1.199.3.4'                                 }
+      let(:bigger) { described_class.new(:number => '1.200.3.4') }
+
+      specify { expect((ip > bigger)).to be_falsey }
+      specify { expect((bigger >= ip)).to be_truthy }
+    end
+
+    context "when 3rd tuple is bigger" do
+      let(:number) { '1.2.2.4'                                 }
+      let(:bigger) { described_class.new(:number => '1.2.3.4') }
+
+      specify { expect((ip > bigger)).to be_falsey }
+      specify { expect((bigger >= ip)).to be_truthy }
+    end
+
+    context "when last tuple is bigger" do
+      let(:number) { '1.2.3.3'                                 }
+      let(:bigger) { described_class.new(:number => '1.2.3.4') }
+
+      specify { expect((ip > bigger)).to be_falsey }
+      specify { expect((bigger >= ip)).to be_truthy }
+    end
   end
 
-  it "knows 1.2.4.0 >= 1.2.3.255" do
-    @ip.number = "1.2.4.0"
-    ip2 = IpRanges::Ip.new :number => '1.2.3.255'
-    expect((@ip >= ip2)).to be_truthy
-  end
+  context "incrementing" do
+    context "when there are no more numbers" do
+      let(:number) { '255.255.255.255' }
 
-  it "knows 1.200.3.4 > 1.199.3.4" do
-    @ip.number = "1.200.3.4"
-    ip2 = IpRanges::Ip.new :number => '1.199.3.4'
-    expect((@ip > ip2)).to be_truthy
-  end
+      it "barfs" do
+        expect { ip.increment }.to raise_error(RuntimeError)
+      end
+    end
 
-  it "knows 1.2.3.4 > 1.2.2.4" do
-    @ip.number = "1.2.3.4"
-    ip2 = IpRanges::Ip.new :number => '1.2.2.4'
-    expect((@ip > ip2)).to be_truthy
-  end
+    context "when 2nd tuple hits 255" do
+      let(:number) { '1.255.255.255' }
 
-  it "knows 1.2.3.4 > 1.2.3.3" do
-    @ip.number = "1.2.3.4"
-    ip2 = IpRanges::Ip.new :number => '1.2.3.3'
-    expect((@ip > ip2)).to be_truthy
-  end
+      specify { expect(ip.increment).to eq("2.0.0.0") }
+    end
 
-  it "knows 1.2.3.3 <= 1.2.3.4" do
-    @ip.number = "1.2.3.3"
-    ip2 = IpRanges::Ip.new :number => '1.2.3.4'
-    expect((@ip > ip2)).to be_falsey
-  end
+    context "when 3rd tuple hits 255" do
+      let(:number) { '1.2.255.255' }
 
-  it "increments when 2nd tuple hits 255" do
-    @ip.number = "1.255.255.255"
-    expect(@ip.increment).to eq("2.0.0.0")
-  end
+      specify { expect(ip.increment).to eq("1.3.0.0") }
+    end
 
-  it "increments when 3rd tuple hits 255" do
-    @ip.number = "1.2.255.255"
-    expect(@ip.increment).to eq("1.3.0.0")
-  end
+    context "when last tuple hits 255" do
+      let(:number) { '1.2.3.255' }
 
-  it "increments when last tuple hits 255" do
-    @ip.number = "1.2.3.255"
-    expect(@ip.increment).to eq("1.2.4.0")
-  end
+      specify { expect(ip.increment).to eq("1.2.4.0") }
+    end
 
-  it "increments last tuple" do
-    @ip.number = '1.2.3.4'
-    expect(@ip.increment).to eq("1.2.3.5")
+    specify { expect(ip.increment).to eq("1.200.3.5") }
   end
 
 end
